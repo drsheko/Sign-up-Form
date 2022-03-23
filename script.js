0b1e7ec6d1b21f87b7a6896164c473dbeff31f01
Shady Israel <drsheko91@gmail.com>
2022-03-23 04:46:49 -0500

    add function to check confirmpassword is matching or not

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 document.querySelector('.accountBtn').addEventListener('click',function(){
 
 document.querySelector('#my-form').className='submitted' ;})
+
+
+function checkPassword(){
+    if (document.querySelector('.password').value != document.querySelector('.confirmPassword').value){
+        document.querySelector('.message').textContent='Your password not matching';
+        return false;
+    }
+    else{document.querySelector('.message').textContent=''
+        return true;
+    }
+}
